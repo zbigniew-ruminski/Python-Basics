c18b6555ea839b634521b68eb04f7deda47b7c00
zbigniew-ruminski <zbi84@o2.pl>
2019-09-24 09:07:26 +0200

    1st ver

--- LAB3.py
+++ LAB3.py
@@ -3,8 +3,12 @@ integer_var = 50
 float_var = 1.5
 bool_var = True
 #
-string_var = "60"
+string_var = 60
 integer_var = 25
 float_var = 1.9
 bool_var = False
 #
+print(string_var)
+print(integer_var)
+print(float_var)
+print(bool_var)
---
+++ LAB_3D.py
@@ -0,0 +1,27 @@
+print(2)
+print(2.1)
+print(-2)
+
+print(2+2)
+print(2+2.2)
+print(2*2)
+print(2**4)
+print(2*(4+4))
+print(10%3)
+#
+my_var = -5
+print(str(my_var))
+#
+print(abs(my_var))
+print(pow(2, 3))
+print(max(4, 6))
+print(max(2, my_var))
+print(min(4, 5))
+print(round(4.7))
+print(round(4.5))
+#
+from math import *
+print(floor(7.8))
+print(ceil(7.8))
+print(sqrt(9))
+#print(sqrt(my_var))
